a9dee2a11c39a81e6875a1a338d7678683350c9d
kihaen baik <kihaen@KMP.local>
2020-03-04 10:45:30 -0500

    Updates

---
+++ precache-manifest.c616c635ebd230a31d644c931dfe0ba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f653acd432e5e0eb7baa3293611634e",
+    "url": "/fanduel-take/index.html"
+  },
+  {
+    "revision": "9850090ff9494627ecf1",
+    "url": "/fanduel-take/static/css/2.6ea3de80.chunk.css"
+  },
+  {
+    "revision": "17daedd93c17211959f0",
+    "url": "/fanduel-take/static/css/main.3d88fe00.chunk.css"
+  },
+  {
+    "revision": "9850090ff9494627ecf1",
+    "url": "/fanduel-take/static/js/2.c5f1d222.chunk.js"
+  },
+  {
+    "revision": "1c4943d9620f88ef4d7f5a0730d7c766",
+    "url": "/fanduel-take/static/js/2.c5f1d222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17daedd93c17211959f0",
+    "url": "/fanduel-take/static/js/main.a76e8350.chunk.js"
+  },
+  {
+    "revision": "b4b446a9248a25ed480f",
+    "url": "/fanduel-take/static/js/runtime-main.6f372203.js"
+  }
+]);
